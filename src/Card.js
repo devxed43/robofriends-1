b73431eb5dad2ea403f11758db2a6d90f56fd858
Edward <edwlope@192-3-36-162-host.colocrossing.com>
2025-05-29 12:23:45 -0700

    destructuring involves passing data to a parent and in the child component, accesing the parents data. from there we take the data and insert it into our jsx

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+// ======== ways of destructuring props ==========
+// props here because we have to recieve from parent
+// const Card = (props) => {
+// const { name, email, id } = props;
+
+const Card = ({ name, email, id }) => {
+  return (
+    <Fragment>
+      <div className="bg-light-green dib br3 pa3 mar2 grow bw2 shadow-5">
+        <img alt="robots" src={`https://robohash.org/${id}`} />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "tachyons"; // npm install tachyons
+import Card from "./Card";
+import { robots } from "./robots";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// index.js holds the parent component
+// we load the data onto the parent
+// in the child, we retrieve the props
+// we can fill in our html with {props.name}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      {/* first robot */}
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+
+      {/* second robot */}
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+
+      {/* third robot */}
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
