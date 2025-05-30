f7f7608be97440b7b968dbcb9ffcc867b2b8fb59
Edward <edwlope@Edwards-MacBook-Pro.local>
2025-05-30 07:50:09 -0700

    simple basic class component styled implementation of searchfield and cardlist filtering based off that searchfield. hard coded robots file and hard coded state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React, { Component } from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import { robots } from "./robots";
-
-// props are things that come out of state
-// parent feeds state into a child component
-// child recieves state, it's a property
-// parent tells it what the state is
+import "./App.css";
 
 class App extends Component {
-  // this constructs our state
-  // this constructs our searchchange
-  //   this constructs all our functionality
   constructor() {
-    // constructs state
-    super(); // calls the constructor of component
+    super();
 
     this.state = {
       robots: robots,
@@ -22,32 +14,21 @@ class App extends Component {
     };
   }
 
-  //   search box functionality
-  // we check this.state.robots
-  // we filter out and return robots name if they're found to be
-  // included in the searchfield
-  //   conclusion: if search field value matches robot array, return
-
-  //   use arrow function so this function is scoped in the app and not confined to the input component
   onSearchChange = (e) => {
-    // we set the state within the search change function
-
-    // we destructure searchfield(key) object from above and give it a value of e.target.value
     this.setState({ searchfield: e.target.value });
+  };
 
+  render() {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log(filteredRobots);
-  };
-
-  render() {
     return (
       <div className="tc">
+        <h1 className="f1">Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
